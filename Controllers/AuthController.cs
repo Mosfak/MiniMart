12df1991cf99f3248f7b31bfefab26b338feee52
Rimon <mosfak.rimon@enosisbd.com>
2025-10-06 04:29:21 +0600

    Implement JWT Authentication

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiniMart.Models;
+using MiniMart.Services;
+
+namespace MiniMart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly JWTService _jwtService;
+
+        //In memory Database
+        private static List<User> _users = new List<User>
+        {
+            new User { Id = 1, Username = "admin", Role = "Admin" },
+            //new User { Id = 2, Username = "customer", Role = "Customer" }
+        };
+
+        public AuthController(JWTService jwtService)
+        {
+            _jwtService = jwtService; 
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            var user = _users.SingleOrDefault(u => u.Username == request.Username);
+            if (user == null)
+            {
+                var newCustomer = new User
+                {
+                    Id = _users.Max(u => u.Id) + 1,
+                    Username = request.Username,
+                    Role = "Customer"
+                };
+                _users.Add(newCustomer);
+                user = newCustomer;
+            }
+
+            if (request.Username == "admin" && request.Password == "Admin123")
+            {
+                var token = _jwtService.GenerateToken(user);
+                return Ok(new { token });
+            }
+
+            
+
+            var customerToken = _jwtService.GenerateToken(user);
+            return Ok(new { token = customerToken });
+        }
+        public class LoginRequest
+        {
+            public string Username { get; set; } = string.Empty;
+            public string? Password { get; set; }
+        }
+    }
+}
---
+++ Services/JWTService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MiniMart.Models;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace MiniMart.Services
+{
+    public class JWTService
+    {
+        private readonly IConfiguration _config;
+        public JWTService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new System.Security.Claims.Claim(System.Security.Claims.ClaimTypes.Name, user.Username),
+                new System.Security.Claims.Claim(System.Security.Claims.ClaimTypes.Role, user.Role)
+            };
+
+            var token = new System.IdentityModel.Tokens.Jwt.JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
