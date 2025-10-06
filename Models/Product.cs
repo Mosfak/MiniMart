646f17f43844589da76782edfd383e4eed95c592
Rimon <mosfak.rimon@enosisbd.com>
2025-10-06 14:13:47 +0600

    Implemented Authorization of user roles

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniMart.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string? category { get; set; } 
+    }
+}
